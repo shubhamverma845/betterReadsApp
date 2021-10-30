aef4288a6ccf2f2e4ceec53590e945e998e53d2d
shubham.verma <shubham.verma@sirionlabs.com>
2021-10-30 23:23:00 +0530

    Added book card with cover

--- src/main/java/io/javabrains/BetterReadsApp.java
+++ src/main/java/io/javabrains/BetterReadsApp.java
@@ -1,13 +1,20 @@
 package io.javabrains;
 
+import io.javabrains.connection.DataStaxAstraProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.file.Path;
+
 @SpringBootApplication
+@EnableConfigurationProperties()
 @RestController
 public class BetterReadsApp {
 
@@ -20,6 +27,10 @@ public class BetterReadsApp {
 		System.out.println(principal);
 		return principal.getAttribute("name");
 	}
-	
 
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties){
+		Path bundle = astraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
+	}
 }
